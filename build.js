0d6952b943a0834c95d4960be6ddda7c117b08ef
ljw1412 <506644791@qq.com>
2022-02-06 16:41:41 +0800

    chore: 新增插件目录一键生成

---
+++ build.js
@@ -0,0 +1,21 @@
+const { glob } = require('glob')
+const { join } = require('path')
+const fs = require('fs')
+const only = require('only')
+
+const PLUGINS_DIR = join(__dirname, 'plugins')
+
+function createPluginList() {
+  const files = glob.sync(`${PLUGINS_DIR}/**/config.json`)
+  console.log(files)
+  const list = files.map((file) => {
+    const config = require(file)
+    return only(config, 'plugin icon name desc version')
+  })
+  fs.writeFileSync(
+    join(PLUGINS_DIR, 'list.json'),
+    JSON.stringify(list, null, 2)
+  )
+}
+
+createPluginList()
