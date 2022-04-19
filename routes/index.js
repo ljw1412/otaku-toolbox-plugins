35191458d68110a3637cd8404dd13ea99a5c6593
ljw1412 <506644791@qq.com>
2022-04-20 00:00:30 +0800

    chore: 新增1个漫画源

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')()
-const { createPluginList } = require('../utils/build-dev')
+const { createPluginList, createACGRuleList } = require('../utils/build-dev')
 
 router.get('/', async (ctx, next) => {
   ctx.body = 'Hello otaku-toolbox-plugins!'
@@ -7,6 +7,7 @@ router.get('/', async (ctx, next) => {
 
 router.get('/create-list', async (ctx, next) => {
   createPluginList()
+  createACGRuleList()
   ctx.body = { message: '生成列表成功！' }
 })
 
--- utils/build-dev.js
+++ utils/build-dev.js
@@ -3,7 +3,22 @@ const { join } = require('path')
 const fs = require('fs')
 const only = require('only')
 
-const PLUGINS_DIR = join(__dirname, '..', 'dev', 'plugins')
+const DEV_DIR = join(__dirname, '..', 'dev')
+const PLUGINS_DIR = join(DEV_DIR, 'plugins')
+
+const acgNames = ['anime', 'comic', 'game']
+
+function createACGItemRuleList(type) {
+  const ACG_DIR = join(DEV_DIR, type)
+  const files = glob.sync(`${ACG_DIR}/!(list).json`)
+  fs.mkdirSync(ACG_DIR, { recursive: true })
+  const list = files.map((file) => {
+    const config = require(file)
+    return only(config, 'name namespace icon url type version')
+  })
+  console.log(list)
+  fs.writeFileSync(join(ACG_DIR, 'list.json'), JSON.stringify(list))
+}
 
 module.exports = {
   createPluginList() {
@@ -17,5 +32,10 @@ module.exports = {
       join(PLUGINS_DIR, 'list.json'),
       JSON.stringify(list, null, 2)
     )
+  },
+  createACGRuleList() {
+    acgNames.forEach((name) => {
+      createACGItemRuleList(name)
+    })
   },
 }
