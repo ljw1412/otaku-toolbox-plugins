2cf891e15ef941ee23d8967e13458ec5568d9ea8
ljw1412 <506644791@qq.com>
2022-04-19 14:23:53 +0800

    chore: 新增源列表

--- build.js
+++ build.js
@@ -3,7 +3,8 @@ const { join } = require('path')
 const fs = require('fs')
 const only = require('only')
 
-const PLUGINS_DIR = join(__dirname, 'release', 'plugins')
+const RELEASE_DIR = join(__dirname, 'release')
+const PLUGINS_DIR = join(RELEASE_DIR, 'plugins')
 
 function createPluginList() {
   const files = glob.sync(`${PLUGINS_DIR}/**/config.json`)
@@ -12,10 +13,25 @@ function createPluginList() {
     const config = require(file)
     return only(config, 'plugin icon name desc version config css')
   })
-  fs.writeFileSync(
-    join(PLUGINS_DIR, 'list.json'),
-    JSON.stringify(list, null, 2)
-  )
+  fs.writeFileSync(join(PLUGINS_DIR, 'list.json'), JSON.stringify(list))
+}
+
+const acgNames = ['anime', 'comic', 'game']
+
+function createACGRuleList(type) {
+  const ACG_DIR = join(RELEASE_DIR, type)
+  const files = glob.sync(`${ACG_DIR}/!(list).json`)
+  fs.mkdirSync(ACG_DIR, { recursive: true })
+  const list = files.map((file) => {
+    const config = require(file)
+    return only(config, 'name namespace icon url type version')
+  })
+  console.log(list)
+  fs.writeFileSync(join(ACG_DIR, 'list.json'), JSON.stringify(list))
 }
 
 createPluginList()
+
+acgNames.forEach((name) => {
+  createACGRuleList(name)
+})
